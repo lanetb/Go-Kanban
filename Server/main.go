64386ba7bfd45d467877aaaa29df6ddfd7d1e555
lanetb <lanetb@dukes.jmu.edu>
2023-09-22 14:14:28 -0400

    created seperate file for db actions

--- Server/main.go
+++ Server/main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"html/template"
 	"log"
-	"os"
 	"net/http"
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -22,30 +19,7 @@ func main() {
 
 	http.HandleFunc("/", h1)
 	
-	log.Println("Connecting to database...")
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	db, err := sql.Open("mysql", os.Getenv("DSN"))
-
-    if err != nil {
-
-        log.Fatalf("failed to connect: %v", err)
-
-    }
-
-    defer db.Close()
-
-    if err := db.Ping(); err != nil {
-
-        log.Fatalf("failed to ping: %v", err)
-
-    }
-
-    log.Println("Successfully connected to PlanetScale!")
+	ConnectToDB()
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
