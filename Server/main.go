421f27ec61841b7d13337c3086a8b8b862f7672c
lanetb <lanetb@dukes.jmu.edu>
2023-09-23 22:16:07 -0400

    got css working

--- Server/main.go
+++ Server/main.go
@@ -30,10 +30,14 @@ func main() {
 		tmpl := template.Must(template.ParseFiles("../Client/html/login.html"))
 		tmpl.Execute(w, nil)
 	}
+
+	fs := http.FileServer(http.Dir("../Client/static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", h1)
 	http.HandleFunc("/register/", h2)
 	http.HandleFunc("/swap-reg/", h3)
-	http.HandleFunc("/login/", h4)
+	http.HandleFunc("/swap-log/", h4)
+
 	ConnectToDB()
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
