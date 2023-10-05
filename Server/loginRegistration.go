f8d1c32e5d2ff9be7b644970e772045323197f3f
lanetb <lanetb@dukes.jmu.edu>
2023-10-05 10:49:29 -0400

    register error

--- Server/loginRegistration.go
+++ Server/loginRegistration.go
@@ -45,33 +45,40 @@ func RegistraitionAuthHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	if len(password) >= 8 && len(password) <= 50 {
+	if len(password) >= 8 && len(password) <= 100 {
 		passLength = true
 	}
 
 	err := db.QueryRow("SELECT username FROM User WHERE username=?", username).Scan(&user)
 	switch {
 		case err == sql.ErrNoRows:
-			if usernameValid && usernameLength && passLower && passUpper  && passNumber && passSpecial && passLength && passNoSpace {
-				hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-				if err != nil {
-					log.Println(err)
+			if usernameValid && usernameLength{
+			    if passLower && passUpper  && passNumber && passSpecial && passLength && passNoSpace {
+					hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+					if err != nil {
+						log.Println(err)
+					}
+					password = string(hashedPassword)
+					log.Println("hash: ", password)
+					_, err = db.Exec("INSERT INTO User (username, password, email) VALUES (?, ?, ?)", username, password, email)
+					if err != nil {
+						log.Println(err)
+					}
+					log.Println("User created")
+					Login(username, password, w, r)
+				} else {
+					tmpl := template.Must(template.ParseFiles("../Client/html/registerbadpassword.html"))
+					tmpl.Execute(w, nil)
 				}
-				password = string(hashedPassword)
-				log.Println("hash: ", password)
-				_, err = db.Exec("INSERT INTO User (username, password, email) VALUES (?, ?, ?)", username, password, email)
-				if err != nil {
-					log.Println(err)
-				}
-				log.Println("User created")
-				Login(username, password, w, r)
 			} else {
-				log.Println("Password invalid")
+				tmpl := template.Must(template.ParseFiles("../Client/html/registerbaduser.html"))
+				tmpl.Execute(w, nil)
 			}
 		case err != nil:
 			log.Println(err)
 		default:
-			log.Println("Username taken")
+			tmpl := template.Must(template.ParseFiles("../Client/html/registerusertaken.html"))
+			tmpl.Execute(w, nil)
 	}
 
 }
@@ -90,45 +97,50 @@ func Login(username string, password string, w http.ResponseWriter, r *http.Requ
 	log.Print("Logging in...")
 	err := db.QueryRow("SELECT UserID, Username, Password FROM User WHERE Username=?", username).Scan(&ID, &user, &pass)
 	if err != nil {
-		log.Println(err)
-	}
-	switch {
-		case err == sql.ErrNoRows:
-			log.Println(err)
-		case err != nil:
-			log.Println(err)
-		default:
-			err := bcrypt.CompareHashAndPassword([]byte(pass), []byte(password))
-			if err != nil {
-				log.Println(err)
-			} else {
-				log.Print("Logged in")
-				var currentUser User
-				currentUser = User{
-					Username: user,
-					ID: ID,
-					Projects: make(map[int]Project),
-				}
-				session, _ := store.Get(r, "session")
-				session.Options.MaxAge = 60 * 60 * 24
-				session.Values["CurrentUser"] = currentUser
-				log.Println("User: ", currentUser.Username)
-				session.Save(r, w)
-
-				currentUser = GetProjects(w, r, session)
-				session.Save(r, w)
-				tmpl, _ := template.ParseFiles("../Client/html/dashboard.html")
-				data := struct{
-					User User 
-					Projects map[int]Project
-				}{
-					User: currentUser,
-					Projects: currentUser.Projects,
-				}
-				err = tmpl.Execute(w, data)
+		template, _ := template.ParseFiles("../Client/html/loginerror.html")
+		template.Execute(w, nil)
+	} else{
+		switch {
+			case err == sql.ErrNoRows:
+				template, _ := template.ParseFiles("../Client/html/loginerror.html")
+				template.Execute(w, nil)
+			case err != nil:
+				template, _ := template.ParseFiles("../Client/html/loginerroruserpass.html")
+				template.Execute(w, nil)
+			default:
+				err := bcrypt.CompareHashAndPassword([]byte(pass), []byte(password))
 				if err != nil {
-					log.Println(err)
+					template, _ := template.ParseFiles("../Client/html/loginerroruserpass.html")
+					template.Execute(w, nil)
+				} else {
+					log.Print("Logged in")
+					var currentUser User
+					currentUser = User{
+						Username: user,
+						ID: ID,
+						Projects: make(map[int]Project),
+					}
+					session, _ := store.Get(r, "session")
+					session.Options.MaxAge = 60 * 60 * 24
+					session.Values["CurrentUser"] = currentUser
+					log.Println("User: ", currentUser.Username)
+					session.Save(r, w)
+
+					currentUser = GetProjects(w, r, session)
+					session.Save(r, w)
+					tmpl, _ := template.ParseFiles("../Client/html/dashboard.html")
+					data := struct{
+						User User 
+						Projects map[int]Project
+					}{
+						User: currentUser,
+						Projects: currentUser.Projects,
+					}
+					err = tmpl.Execute(w, data)
+					if err != nil {
+						log.Println(err)
+					}
 				}
-			}
+		}
 	}
 }
